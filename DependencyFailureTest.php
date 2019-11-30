5f63713bc49e740ea774680f2044169ad7c18af8
Sigit-Wasis <sigitghoticmetal2001@gmail.com>
2019-11-30 12:07:27 +0700

    cicd

---
+++ DependencyFailureTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @depends testOne
+     */
+    public function testTwo()
+    {
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+function testCi() {
+	$a = 8;
+	$b = 9;
+
+	return($a + $b);
+}
